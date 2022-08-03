9a21fb66b8a5d3f3aa73e3dc598250fe31ae042e
Akhil <inamdar.akhil@capgemini.com>
2022-08-03 13:57:52 +0530

    first commit

---
+++ src/main/java/com/Online_Vegetable_Shopping/OnlineVegetableShoppingApplication.java
@@ -0,0 +1,13 @@
+package com.Online_Vegetable_Shopping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OnlineVegetableShoppingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OnlineVegetableShoppingApplication.class, args);
+	}
+
+}
